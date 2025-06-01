df2c10f1857857bfb7e4af26242cc585e657dcf7
eungyu <beg4660@naver.com>
2025-06-01 23:57:06 +0900

    refactor: 백그라운드 컴포넌트 className 속성 추가가

--- src/components/layout/Background.tsx
+++ src/components/layout/Background.tsx
@@ -2,15 +2,17 @@ import { clsx } from "clsx";
 
 interface BackgroundProps {
   backgroundImage: string;
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  backgroundClassName?: string;
 }
 
-export function Background({ backgroundImage, children }: BackgroundProps) {
-  return ~~(
+export function Background({ backgroundImage, children, backgroundClassName }: BackgroundProps) {
+  return (
     <div className="w-screen h-screen bg-black font-TJ overflow-hidden flex justify-center items-center">
       <div
         className={clsx(
-          "relative w-[360px] h-[258px] sm:w-[430px] sm:h-[300px] md:w-[615px] md:h-[430px] xl:w-[1180px] xl:h-[820px] bg-contain bg-center bg-no-repeat"
+          "relative w-[360px] h-[258px] sm:w-[430px] sm:h-[300px] md:w-[615px] md:h-[430px] xl:w-[1180px] xl:h-[820px] bg-contain bg-center bg-no-repeat",
+          backgroundClassName
         )}
         style={{
           backgroundImage: `url(${backgroundImage})`,
