63e0a967f606d40584a723c62b8fa1b0ef3fa4d6
eungyu <beg4660@naver.com>
2025-06-02 00:01:53 +0900

    ui: 아기돼지 삼형제에 쓰는 컴포넌트 추가

---
+++ src/module/investing-game/component/little-pig-component/pink-border-modal.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react";
+import sirenPig from "@/assets/image/investing-game/little_pig/little_siren_pig.webp";
+
+interface PinkBorderModalProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export const PinkBorderModal = ({ children, className = "" }: PinkBorderModalProps) => {
+  return (
+    <div className="relative flex items-center justify-center w-[33.5rem] h-[21.5rem] bg-main-pink-500 border-2 sm:border-5 border-main-brown-550 rounded-4xl">
+      <div
+        className={`relative px-10 w-[32rem] h-[20rem] bg-white border-2 sm:border-5 border-main-brown-550 rounded-3xl ${className}`}
+      >
+        {children}
+        <img src={sirenPig} alt="siren pig" className="absolute -bottom-6 -right-7 min-w-0 h-20 object-contain" />
+      </div>
+    </div>
+  );
+};
