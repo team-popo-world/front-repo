9ff9b95e211fea6a21449d3a3a553182a9774a48
eungyu <beg4660@naver.com>
2025-06-03 21:56:30 +0900

    feat: 뒤로가기 버튼 추가, 모달 패딩 변경, 노랑 모달 박스 추가

---
+++ src/components/button/BackArrow.tsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import backArrow from "@/assets/image/common/back_arrow.webp";
+import { clsx } from "clsx";
+interface BackArrowProps {
+  onClick?: () => void;
+  className?: string;
+}
+
+export const BackArrow = ({ onClick, className }: BackArrowProps) => {
+  const navigate = useNavigate();
+  return (
+    <img
+      src={backArrow}
+      alt="backArrow"
+      className={clsx(className ? className : "z-[100] absolute top-3 left-3 w-8 h-8 object-contain")}
+      onClick={() => (onClick ? onClick() : navigate(-1))}
+    />
+  );
+};
