08ebf02e957b7eecaa3455175c55638886427031
jjeunv <jjeunv@gmail.com>
2025-06-06 17:50:54 +0900

    ui: 퀘스트 첫 페이지 페이지, 템플릿 추가

--- src/module/quest/template/index.tsx
+++ src/module/quest/template/index.tsx
@@ -1,16 +1,19 @@
+import { BackArrow } from "@/components/button/BackArrow";
 import { Background } from "../../../components/layout/Background";
 import { TextWithStroke } from "@/components/text/TextWithStroke";
 
 interface QuestTemplateProps {
   onClickQuest?: (quest: string) => void;
+  onBack: () => void;
 }
-export const QuestTemplate = ({ onClickQuest }: QuestTemplateProps) => {
+export const QuestTemplate = ({ onClickQuest, onBack }: QuestTemplateProps) => {
   return (
     // 배경 이미지
     <Background
       aria-label="퀘스트 맵 화면 배경 이미지"
       backgroundImage="https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749002225/quest-map-page.webp"
     >
+      <BackArrow onClick={onBack} />
       {/* 부모 퀘스트 집 이미지, 일일 퀘스트 시계탑 이미지 및 텍스트 */}
       <img
         src="https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749003144/quest-map-page-parent.webp"
@@ -29,7 +32,7 @@ export const QuestTemplate = ({ onClickQuest }: QuestTemplateProps) => {
         alt="일일 퀘스트 이미지"
         className="absolute h-[15rem] right-[0.7rem] top-[0.1rem] cursor-pointer "
       />
-      <div className="absolute xl:left-[33.5rem] top-[9.6rem] md:left-[33.2rem]">
+      <div className="absolute xl:left-[33.5rem] top-[9.6rem] md:left-[33.2rem] sm:left-[33.1rem]">
         <TextWithStroke
           text="일일"
           textClassName="text-main-yellow-300 text-[0.9rem] "
@@ -38,7 +41,6 @@ export const QuestTemplate = ({ onClickQuest }: QuestTemplateProps) => {
       </div>
 
       {/* 포니 */}
-      {/* 부모 포니 */}
       <img
         src="https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749014097/quest-map-parents-popo.webp"
         alt="부모퀘스트 포니"
@@ -49,6 +51,12 @@ export const QuestTemplate = ({ onClickQuest }: QuestTemplateProps) => {
         alt="일일퀘스트 포니"
         className="absolute h-22 right-[3.8rem] top-[11rem]"
       />
+
+      <img
+        src="https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749014888/image-Photoroom_5_calb9b.webp"
+        alt="고민하는 포니"
+        className="absolute h-[10rem] left-[14.5rem] xl:top-[16.97rem] md:top-[16.8rem] sm:top-[16.8rem]"
+      />
       {/* 제목 - 퀘스트 */}
       <img
         src="https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749009914/quest-map-title-.webp"
@@ -58,14 +66,8 @@ export const QuestTemplate = ({ onClickQuest }: QuestTemplateProps) => {
       <TextWithStroke
         text="퀘스트"
         className="absolute left-[15.9rem] top-[1.6rem]"
-        textClassName="text-main-yellow-800 text-[2.5rem] "
+        textClassName="text-main-yellow-350 text-[2.5rem] "
         strokeClassName="text-main-brown-700 text-[2.5rem] text-stroke-width-[0.4rem] text-stroke-color-main-brown-800"
-      />
-      {/* 포니 */}
-      <img
-        src="https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749014888/image-Photoroom_5_calb9b.webp"
-        alt="고민하는 포니"
-        className="absolute h-40 left-[14.5rem] xl:top-[16.97rem] md:top-[16.8rem]"
       />
 
       {/* 표지판 속 텍스트 */}
@@ -85,13 +87,13 @@ export const QuestTemplate = ({ onClickQuest }: QuestTemplateProps) => {
       {/* 일일 퀘스트 */}
       <TextWithStroke
         text="일일"
-        className="absolute left-[8rem] top-[12.8rem] md:left-[7.8rem] "
+        className="absolute left-[8rem] top-[12.8rem] md:left-[7.8rem] sm:left-[7.7rem] "
         textClassName="text-main-yellow-300 text-[1.5rem] "
         strokeClassName="text-main-brown-600 text-[1.5rem] text-stroke-width-[0.3rem] text-stroke-color-main-brown-800"
       />
       <TextWithStroke
         text="퀘스트"
-        className="absolute left-[4.5rem] top-[14.35rem] md:left-[4.3rem]"
+        className="absolute left-[4.5rem] top-[14.35rem] md:left-[4.3rem] sm:left-[4.2rem]"
         textClassName="text-main-yellow-300 text-[1.5rem] "
         strokeClassName="text-main-brown-600 text-[1.5rem] text-stroke-width-[0.3rem] text-stroke-color-main-brown-800"
       />
