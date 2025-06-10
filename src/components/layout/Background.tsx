ee43abd185efda671a05f20e988f021c7e9e7c40
hyungee <hyungeerhee@naver.com>
2025-06-10 09:54:22 +0900

    fix: background.tsx 수정

--- src/components/layout/Background.tsx
+++ src/components/layout/Background.tsx
@@ -11,7 +11,7 @@ export function Background({
   backgroundClassName,
 }: BackgroundProps) {
   return (
-    <div className="w-screen h-screen bg-black font-TJ overflow-visible flex justify-center items-center">
+    <div className="w-screen h-screen bg-black font-TJ overflow-hidden flex justify-center items-center">
       <div
         className={clsx(
           "relative w-[360px] h-[258px] sm:w-[430px] sm:h-[300px] md:w-[615px] md:h-[430px] xl:w-[1180px] xl:h-[820px] bg-contain bg-center bg-no-repeat",
