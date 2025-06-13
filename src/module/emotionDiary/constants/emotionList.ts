bd0843d5f8eb8419b5872b9ffee84780707a06ea
jjeunv <jjeunv@gmail.com>
2025-06-13 15:19:15 +0900

    feat: 감정리스트 추가

---
+++ src/module/emotionDiary/constants/emotionList.ts
@@ -0,0 +1,32 @@
+export const emotionList = [
+  {
+    url: "https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749623811/emotion-happy_b57kwc.webp",
+    label: "기쁨",
+    server: "HAPPY",
+  },
+  {
+    url: "https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749623428/emotion-angry_yrpv3l.webp",
+    label: "화남",
+    server: "ANGRY",
+  },
+  {
+    url: "https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749623512/emotion-sad_ahoplf.webp",
+    label: "슬픔",
+    server: "SAD",
+  },
+  {
+    url: "https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749623755/emotion-soso_n5tczw.webp",
+    label: "평온",
+    server: "CALM",
+  },
+  {
+    url: "https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749623669/emotion-love_zljlxe.webp",
+    label: "사랑",
+    server: "LOVE",
+  },
+  {
+    url: "https://res.cloudinary.com/dgmbxvpv9/image/upload/v1749623606/emotion-embarrassed_npzyuk.webp",
+    label: "당황",
+    server: "CONFUSED",
+  },
+] as const;
