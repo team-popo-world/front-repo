e0eb33297b0abd04ddb272837af04acf4cbabf88
jjeunv <jjeunv@gmail.com>
2025-06-16 09:48:02 +0900

    ui: 투자 ui 변경

--- src/module/investing/template/index.tsx
+++ src/module/investing/template/index.tsx
@@ -1,32 +1,41 @@
 import { TextWithStroke } from "../../../components/text/TextWithStroke";
 import { Info } from "../components/Info";
 import { Background } from "../../../components/layout/Background";
+import { IMAGE_URLS } from "@/lib/constants/constants";
+import { BackArrow } from "@/components/button/BackArrow";
+import type { AnimationControls } from "framer-motion";
+import { motion } from "framer-motion";
 
 interface InvestingTemplateProps {
-  onClickChapter?: (chapter: string) => void;
+  onBack: () => void;
+  onChapterClick: (chapter: string) => void;
+  animation: AnimationControls;
+  onAnimationComplete: () => void;
+  point: number | null;
 }
 
 export const InvestingTemplate = ({
-  onClickChapter,
+  onBack,
+  animation,
+  onChapterClick,
+  onAnimationComplete,
+  point,
 }: InvestingTemplateProps) => {
   return (
     // 백그라운드 이미지
-    <Background
-      backgroundImage={
-        "https://res.cloudinary.com/dgmbxvpv9/image/upload/v1748949363/investing_map_page.webp"
-      }
-    >
+    <Background backgroundImage={IMAGE_URLS.investing.bg}>
+      <BackArrow onClick={onBack} />
       {/* 모의투자 제목 + 보유 코인 div */}
       <div
         aria-label="페이지 제목과 보유 코인 정보 섹션"
-        className="flex flex-col justify-center items-center mt-7"
+        className="flex flex-col justify-center items-center mt-[1.3rem]"
       >
         {/* 모의투자 제목 */}
         <div aria-label="페이지 제목: 모의투자">
           <TextWithStroke
             text="모의투자"
-            textClassName="text-main-yellow-800 text-[2rem]"
-            strokeClassName="text-main-brown-800 text-[2rem] text-stroke-width-[0.4rem] text-stroke-color-main-brown-800"
+            textClassName="text-main-yellow-800 text-[2.1rem]"
+            strokeClassName="text-main-brown-800 text-[2.1rem] text-stroke-width-[0.4rem] text-stroke-color-main-brown-800"
           />
         </div>
         {/* 보유 코인 */}
@@ -41,7 +50,7 @@ export const InvestingTemplate = ({
               className="h-[1.5rem]"
             />
             <TextWithStroke
-              text="2000냥"
+              text={`${point?.toString()}냥`}
               className="ml-[0.2rem]"
               textClassName="text-main-yellow-800 text-[1.1rem]"
               strokeClassName="text-main-brown-800 text-[1.1rem] text-stroke-width-[0.2rem] text-stroke-color-main-brown-800"
@@ -51,64 +60,64 @@ export const InvestingTemplate = ({
       </div>
 
       {/* 포니 */}
-      <img
-        src="https://res.cloudinary.com/dgmbxvpv9/image/upload/v1748982403/investing-poni.webp"
+      <motion.img
+        src={IMAGE_URLS.investing.poni}
         alt="포니 캐릭터"
-        className="h-[11.25rem] absolute left-[14.75rem] top-[8.75rem]"
+        className="absolute h-[11.25rem] left-[14.5rem] top-[9rem]"
+        animate={animation}
+        transition={{ duration: 1 }}
+        onAnimationComplete={onAnimationComplete}
       />
       {/* 챕터 제목, 가격 */}
       <TextWithStroke
         text="아기돼지 삼형제"
-        className="absolute left-[3.1rem] bottom-[5.45rem]"
+        className="absolute left-[2.7rem] bottom-[3.85rem]"
         textClassName="text-main-yellow-150 text-[1.3rem]"
         strokeClassName="text-investing-orange-100 text-[1.3rem] text-stroke-width-[0.6rem] text-stroke-color-main-brown-800"
       />
-      <Info price="700냥" className="absolute left-[5.1rem] top-[3.9rem]" />
+      <Info price="700냥" className="absolute left-[4.3rem] top-[1.7rem]" />
       <TextWithStroke
         text="푸드트럭 왕국"
-        className="absolute left-[18.7rem] bottom-[5.45rem]"
+        className="absolute left-[18.7rem] bottom-[3.85rem]"
         textClassName="text-main-yellow-150 text-[1.3rem]"
         strokeClassName="text-investing-yellow-100 text-[1.3rem] text-stroke-width-[0.6rem] text-stroke-color-main-brown-800"
       />
-      <Info price="1000냥" className="absolute right-[4.6rem] top-[3.75rem]" />
+      <Info price="1000냥" className="absolute right-[4.6rem] top-[1.7rem]" />
       <TextWithStroke
         text="마법 왕국"
-        className="absolute right-[11.5rem] top-[14.75rem]"
+        className="absolute right-[12.1rem] top-[15.5rem]"
         textClassName="text-main-yellow-150 text-[1.3rem]"
         strokeClassName="text-investing-purple-100 text-[1.3rem] text-stroke-width-[0.6rem] text-stroke-color-main-brown-800"
       />
-      <Info price="700냥" className="absolute left-[5.1rem] bottom-[1.45rem]" />
+      <Info price="700냥" className="absolute left-[4.5rem] bottom-[1rem]" />
       <TextWithStroke
         text="달빛 도둑"
-        className="absolute left-[6.75rem] top-[14.75rem]"
+        className="absolute left-[7.4rem] top-[15.5rem]"
         textClassName="text-main-yellow-150 text-[1.3rem]"
         strokeClassName="text-investing-blue-100 text-[1.3rem] text-stroke-width-[0.6rem] text-stroke-color-main-brown-800"
       />
-      <Info
-        price="2000냥"
-        className="absolute right-[4.9rem] bottom-[1.45rem]"
-      />
+      <Info price="2000냥" className="absolute right-[4rem] bottom-[1rem]" />
 
       {/* 챕터 클릭 포지션 */}
       <div
         aria-label="아기돼지 삼형제"
         className="w-[12.1rem] h-[12rem] absolute left-[1.5rem] top-[1.9rem] cursor-pointer"
-        onClick={() => onClickChapter?.("little-pig")}
+        onClick={() => onChapterClick("little-pig")}
       ></div>
       <div
         aria-label="푸드트럭 왕국"
         className="w-[12.1rem] h-[12rem] absolute right-[1.5rem] top-[1.9rem] cursor-pointer"
-        onClick={() => onClickChapter?.("chapter2")}
+        onClick={() => onChapterClick?.("truck")}
       ></div>
       <div
         aria-label="마법 왕국"
         className="w-[12.1rem] h-[12rem] absolute left-[1.5rem] top-[14.1rem] cursor-pointer"
-        onClick={() => onClickChapter?.("chapter3")}
+        onClick={() => onChapterClick?.("masic")}
       ></div>
       <div
         aria-label="달빛 도둑"
         className="w-[12.1rem] h-[12rem] absolute right-[1.5rem] top-[14.1rem] cursor-pointer"
-        onClick={() => onClickChapter?.("chapter4")}
+        onClick={() => onChapterClick?.("ninja")}
       ></div>
     </Background>
   );
